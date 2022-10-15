19ae48fa51035b20b3dde668776e39d310eff4e6
Sveta Andreeva <89111378147v@yandex.com>
2022-10-15 09:07:44 +0300

    added name

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -3,6 +3,6 @@ import org.junit.jupiter.api.Test;
 public class SimpleTest {
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello world");
+        System.out.println("Hello from Света");
     }
 }
